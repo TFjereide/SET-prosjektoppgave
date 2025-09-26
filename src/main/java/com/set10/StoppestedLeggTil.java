8a4857dc5ac95a9966055b6a78209ab65e39d269
Halle02 <adrianarmand@gmail.com>
2025-09-26 17:30:43 +0200

    Objektet "Stoppesteder" er lagt til og metode for å legge til stoppesteder i en liste.

---
+++ src/main/java/com/set10/StoppestedLeggTil.java
@@ -0,0 +1,21 @@
+package com.set10;
+import java.util.ArrayList;
+
+public class StoppestedLeggTil {
+    private ArrayList<Stoppested> stoppesteder = new ArrayList<>();
+
+
+    public void leggTilStoppested(int id, String adresse) {
+        stoppesteder.add(new Stoppested(id, adresse));
+    }
+
+
+    public void visStoppesteder() {
+        for (Stoppested s : stoppesteder) {
+            System.out.println(s);
+        }
+    }
+
+
+
+}
