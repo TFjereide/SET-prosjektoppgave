504303a39a71209bbb6696efc3c455b7eeacca2d
julie5280 <stordaljulie@gmail.com>
2025-10-23 09:14:41 +0200

    Update Bruker.java
    kunne kjøpe bilett

--- src/main/java/com/set10/core/Bruker.java
+++ src/main/java/com/set10/core/Bruker.java
@@ -1,11 +1,12 @@
 package com.set10.core;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 public class Bruker {
     public int brukerId;
     public String navn;
     public ArrayList<Billett> aktiveBilletter;
-    public ArrayList<Billett> gamleBiletter;
+    public ArrayList<Billett> gamleBilletter;
 
     private static int nesteLedigId = 1;
 
@@ -13,9 +14,18 @@ public class Bruker {
         this.brukerId = nesteLedigId++;
         this.navn = navn;
         this.aktiveBilletter = new ArrayList<>();
-        this.gamleBiletter = new ArrayList<>();
+        this.gamleBilletter = new ArrayList<>();
 
     }
+    public void kjopBillett(String type, LocalDateTime startTid) {
+    Billett nyBillett = new Billett(type, startTid);
+    aktiveBilletter.add(nyBillett);
+    System.out.println(navn + " kjøpte en " + type + " billett.");}
+
+    public void kjopBillettTilAnnenBruker(String type, LocalDateTime startTid, Bruker mottaker) {
+    Billett nyBillett = new Billett(type, startTid);
+    mottaker.aktiveBilletter.add(nyBillett);
+    System.out.println(navn + " kjøpte " + type + "billett til " + mottaker.navn);}
 
     @Override
     public String toString(){
