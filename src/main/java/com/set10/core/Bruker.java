a3b1f0d28db823a91a974cd3eb6e6992db24f5c4
julie5280 <stordaljulie@gmail.com>
2025-10-26 00:19:12 +0200

    Update Bruker.java
    oppdatere små feil

--- src/main/java/com/set10/core/Bruker.java
+++ src/main/java/com/set10/core/Bruker.java
@@ -2,34 +2,44 @@ package com.set10.core;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+
 public class Bruker {
-    public int brukerId;
+    public int id;
     public String navn;
     public ArrayList<Billett> aktiveBilletter;
     public ArrayList<Billett> gamleBilletter;
-
-    private static int nesteLedigId = 1;
-
-    public Bruker(String navn){
-        this.brukerId = nesteLedigId++;
+    
+    public Bruker(int id, String navn) {
+        this.id = id;
         this.navn = navn;
         this.aktiveBilletter = new ArrayList<>();
         this.gamleBilletter = new ArrayList<>();
-
     }
+
+    public Bruker(String navn) {
+        this.navn = navn;
+        this.aktiveBilletter = new ArrayList<>();
+        this.gamleBilletter = new ArrayList<>();
+    }
+
     public void kjopBillett(String type, LocalDateTime startTid) {
-    Billett nyBillett = new Billett(type, startTid);
-    aktiveBilletter.add(nyBillett);
-    System.out.println(navn + " kjøpte en " + type + " billett.");}
+        Billett nyBillett = new Billett(type, startTid);
+        aktiveBilletter.add(nyBillett);
+        System.out.println(navn + " kjøpte en " + type + " billett.");
+    }
 
     public void kjopBillettTilAnnenBruker(String type, LocalDateTime startTid, Bruker mottaker) {
-    Billett nyBillett = new Billett(type, startTid);
-    mottaker.aktiveBilletter.add(nyBillett);
-    System.out.println(navn + " kjøpte " + type + "billett til " + mottaker.navn);}
+        Billett nyBillett = new Billett(type, startTid);
+        mottaker.aktiveBilletter.add(nyBillett);
+        System.out.println(navn + " kjøpte " + type + " billett til " + mottaker.navn);
+    }
 
     @Override
-    public String toString(){
-         return "BrukerId: " + brukerId + " Navn: " + navn;
+    public String toString() {
+        return "BrukerId: " + id + " Navn: " + navn;
+    }
 
+    public String hentVisningsnavn() {
+        return navn;
     }
 }
